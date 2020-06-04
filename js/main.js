d2f9d156f116e7ece4b6bc535d303e7f91fd4aa6
Alesya Landar <lesyalandar@gmail.com>
2020-06-04 20:54:48 +0300

    change

--- js/main.js
+++ js/main.js
@@ -1,134 +1,186 @@
-  console.log('Sample JavaScript #2 HW #16');
-
-// // * #1
-var userObj = {
-    firstName: 'Энтони',
-    lastName: 'Хопкинс',
-    age: 60,
-    fullName () {
-        return `${userObj.firstName} ${userObj.lastName}`;
+// ##1
+var counter = (function () {
+    var count = 0;
+    return function (n) {
+    count = n !== undefined ? n : count;
+    return count++;
     }
- }
- console.log(userObj);
+    }());
+       
+    console.log(counter(7));
+    console.log(counter(10));
+    console.log(counter());
+
+
+
+    // ##2
+
+ var counting = (function () {
+    var count = 0;
+    return {
+    value(n) {
+    if (n !== undefined) count = n;
+    return count;
+    },
+    increment () {
+    count++;
+    },
+    decrement () {
+    count--;
+        }
+    };
+   }());
+
+
+    console.log(counting.value());
+    console.log(counting.value(5));
+    counting.increment();
+    counting.increment();
+    console.log(counting.value());
+    counting.decrement();
+    console.log(counting.value());
+    console.log(counting.value(77));
+    counting.increment();
+    counting.increment();
+    console.log(counting.value());gulp watch
+
+
+    // ##3
+
  
-
-// // //  * #2
-
- console.log(userObj.fullName());
- 
-
-// // //  * #3
- function defUpperStr(f1) {
-     return `${f1 || 'DEFAULT TEXT'}`.toUpperCase();
- }
-
-defUpperStr('My text');
-console.log(defUpperStr('My text'))
-console.log(defUpperStr());
-
-
-// //  * #4
-
-
-function evenFn(n) {
-    var mas = [];
-    for (var i = 1; i <= n ; i++) 
-        if (i % 2 === 0) 
-        mas.push(i);
-        return mas;
-    
-}
-     evenFn(20)
-    
-
-
-
-// //  * #5
-
-function weekFn(n) {
-    var day;
-
-    switch (n) {
-        case 1: day = 'Понедельник'; break;
-        case 2: day = 'Вторник'; break;
-        case 3: day = 'Среда'; break;
-        case 4: day = 'Четверг'; break;
-        case 5: day = 'Пятница'; break;
-        case 6: day = 'Суббота'; break;
-        case 7: day = 'Воскресенье'; break;
-
-        default: day = null;  
-    }  
-    return day;
+    function myPow(a, b, callback) {
+        var res = function (x,n){
+        if (n!== 1) { return x *= res(x,n - 1); }
+        else { return x; }
+        };
+       return callback (a,b,res(a,b));
+           
 }
 
-console.log(weekFn(2));
-
-
-
-
-// // * #6
-
-function ageClassification(n) {
-    var x = n < 0 ? null : n >= 0 && n <= 24 ? 'детский возраст' : n > 24 && n <= 44 ? 'молодой возраст' : n > 44 && n <= 65 ? 'средний возраст' :
- n > 65 && n <= 75 ? 'пожилой возраст': n > 75 && n <= 90 ? 'старческий возраст' : n > 90 && n <= 122 ? 'долгожители' : null;
- 
-    
-    return x;
- }
- 
-//  console.log('-1 :', ageClassification(-1)); // -1 : null
-// console.log('5 :', ageClassification(5)); // 5 : детский возраст
-// console.log('34 :', ageClassification(34)); // 34 : молодой возраст
-// console.log('50 :', ageClassification(50)); // 50 : средний возраст
-// console.log('65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
-// console.log('80 :', ageClassification(80)); // 80 : старческий возраст
-// console.log('110 :', ageClassification(110)); // 110 : долгожители
-// console.log('130 :', ageClassification(130)); // 130 : null
-
-
-
-
-// // // * #7
-function oddFn(n) {
-
-var mas2 = [];
-var i = 0; 
-    while (i++ < n)  if (i % 2 !== 0)
-        mas2.push(i);
-        return mas2;
-}
-
- console.log(oddFn(20));
+function myPrint(a,b,res)
+{
+    return a + '^' + b + '=' + res;
    
-
-
-
-// // *8
-
-function mainFunc(a, b, func) {
-if (typeof func != 'function')
-return false;
-return func (a,b);
 }
 
-
-function cbPow (a, b){
-    return Math.pow(a,b);
-}
+console.log(myPow(2,4,myPrint));
+     
 
 
-function cbAdd (a, b){
-    return a+b;
-}
 
-function cbRandom (a,b){
-    return Math.ceil(Math.random() * (a - b) + b );
-}
+// ##4 ##5 ##6
 
-console.log(mainFunc(2, 5,cbAdd));
-console.log(mainFunc(2, 5,cbPow));
-console.log(mainFunc(2, 5,cbRandom));
+function info() {
+  return this.name + ' ' + this.model + ', ' +  this.engine + 'cc' + ', ' + 'year ' + this.year + ', ' + this.used;
+};
+
+var yearNow = 2020;
+
+var car = {
+    engine: 1500,
+    model: 'Rio',
+    name: 'Kia',
+    year: 2013,
+    get used(){
+        return this.year === yearNow ? 'new' : 'used';
+        },
+        set used(value) {
+    if (value = 'new') {
+        this.year = yearNow;
+    } else {
+        this.year = this.year;
+    }
+    },
+    info: info
+};
+
+var car1 = {
+    engine: 2000,
+    model: 'RAV-4',
+    name: 'Toyota',
+    year: 2019,
+    get used(){
+        return this.year === yearNow ? 'new' : 'used';
+        },
+        set used(value) {
+    if (value = 'new') {
+        this.year = yearNow;
+    } else {
+        this.year = this.year;
+    }
+    },
+    info: info
+};
+
+var car2 = {
+    engine: 1700,
+    model: 'Golf',
+    name: 'Volksvagen',
+    year: 1999,
+    get used(){
+        return this.year === yearNow ? 'new' : 'used';
+        },
+        set used(value) {
+    if (value = 'new') {
+        this.year = yearNow;
+    } else {
+        this.year = this.year;
+    }
+    },
+    info: info
+};
  
- 
+console.log(car.info());
 
+
+car.used = 'new';
+
+console.log(car.used);
+console.log(car.year);
+
+
+// ##7
+
+var value = [3, 7, 55, 77, 133, 99, 30];
+
+function myMax(arr) {
+    return Math.max.apply(null, arr);
+};
+console.log(myMax(value));
+
+// ##8
+
+
+function myMul(a, b) {
+    return a * b;
+};
+console.log(myMul(4,9));
+
+var myDouble = myMul.bind(null, 2);
+console.log(myDouble(2));
+
+var myTriple = myMul.bind(null, 3);
+console.log(myTriple(3));
+
+
+
+// ##9
+
+var list = [
+    'html',
+    'css',
+    'js',
+    'css',
+    'hard_js',
+    'hard_js'
+];
+function myUniq(arr) {
+var set = new Set;
+
+arr.forEach ((array) => {
+    set.add(array);  
+});
+return set;
+};
+
+console.log(myUniq(list));
